436ab83e73a328b24cfd1901c601aa5afd920634
Bruno Vinicius Scodeller Silva <36263938+blindado25@users.noreply.github.com>
2023-07-16 18:26:09 -0300

    Add files via upload

---
+++ Desafio Orkut/Script/script.js
@@ -0,0 +1,15 @@
+function smiley() {
+    alert("Confiável?");
+  }
+
+  function star() {
+    alert("Fã?");
+  }
+
+  function legal() {
+    alert("Legal?");
+  }
+
+  function sexy() {
+    alert("Sexy?");
+  }
