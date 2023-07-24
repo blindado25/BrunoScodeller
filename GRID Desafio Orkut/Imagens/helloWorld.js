fe76af1b01fadfb5a174e709894ff208e50e7b27
Bruno Vinicius Scodeller Silva <36263938+blindado25@users.noreply.github.com>
2023-07-24 01:46:13 -0300

    Add files via upload

---
+++ GRID Desafio Orkut/Imagens/helloWorld.js
@@ -0,0 +1,54 @@
+// variável
+// declarar uma varíavel
+// const => constante
+// let => pode ser trocado
+// x = 0
+// tipos implícitos - tipos inferidos
+
+// declarando variáveis
+const cor = "#000000";
+const maoridade = 18;
+const teste = true;
+const teste2 = false;
+const teste3 = undefined;
+const teste4 = null;
+
+// não pode trocar o valor de varíavel const
+// teste4 = 1;
+
+// declarando a variável
+let teste5 = false;
+// trocando o valor da variável
+teste5 = true;
+// usando a variável
+console.log(teste5);
+
+// tipos de dados
+// tipos primitivos
+// string (texto) => "" aspas duplas, '' aspas simples
+// number (número) => 18, 1, 4
+// boolean (booleano) => true, false
+// undefined => undefined
+// null => null
+
+// tipos complexos
+// object (objeto) => {}
+// array (lista) => []
+
+// comparação
+// == ou === => igualdade
+// 1 == 2 => false
+// 1 == 1 => true
+// != ou !== => desigualdade
+// maior ou menor => < >
+// >= maior ou igual
+// <= menor ou igual
+
+const a = 10;
+const b = 2;
+const c = 10;
+
+console.log("comparação", a >= c);
+
+// operadores lógicos
+// funções
